87abef506033d4c958b4126b156c8f923cd120fb
Braden M. Kelley <redbmk@gmail.com>
2019-02-10 19:17:51 -0800

    add search box

--- src/search.js
+++ src/search.js
@@ -1,3 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { AsyncTypeahead } from 'react-bootstrap-typeahead';
 
-export default () => <div>TODO: Search</div>;
+import { searchShows, selectShow } from './actions';
+
+const Search = ({ shows, fetchShows, selectShow, onChange }) => {
+  const [value, setValue] = useState('');
+  const options = (shows.search[value] || []).map(id => shows.detail[id]);
+
+  return (
+    <AsyncTypeahead
+      autoFocus
+      labelKey="name"
+      filterBy={() => true}
+      onSearch={term => {
+        fetchShows(term);
+        setValue(term);
+      }}
+      onChange={selectShow}
+      placeholder="Search for a TV show..."
+      isLoading={!!value && !shows.search[value]}
+      options={options}
+    />
+  );
+};
+
+const mapStateToProps = state => ({
+  shows: state.shows,
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetchShows: value => searchShows(value).then(dispatch),
+  selectShow: ([show]) => dispatch(selectShow(show && show.id)),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Search);
