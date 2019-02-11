eae045cdfd0b760d64bea284dbfbe37e494ad41f
Braden M. Kelley <redbmk@gmail.com>
2019-02-10 19:39:39 -0800

    add show detail

--- src/detail.js
+++ src/detail.js
@@ -1,3 +1,31 @@
 import React from 'react';
+import ReactHtmlParser from 'react-html-parser';
+import { connect } from 'react-redux';
 
-export default () => <div>TODO: Detail</div>;
+const Detail = ({ show }) =>
+  show ? (
+    <div className="media">
+      <img
+        className="align-self-start mr-3"
+        width={200}
+        src={show.image.original}
+        alt={show.name}
+      />
+      <div className="media-body">
+        <h5 className="mt-0">
+          {show.name}
+          {console.log(show)}
+          {show.network && <small className="ml-2">{show.network.name}</small>}
+        </h5>
+        {ReactHtmlParser(show.summary)}
+      </div>
+    </div>
+  ) : (
+    <div>Select a show to view detail</div>
+  );
+
+const mapStateToProps = ({ shows }) => ({
+  show: shows.detail[shows.selected],
+});
+
+export default connect(mapStateToProps)(Detail);
