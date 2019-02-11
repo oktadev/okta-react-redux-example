f9a59a9a8fa175a73507c6eabd80efe061f4da9c
Braden M. Kelley <redbmk@gmail.com>
2019-02-11 13:58:05 -0800

    add authentication

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Security, ImplicitCallback, withAuth } from '@okta/okta-react';
 
 import { Provider } from 'react-redux';
 import store from './store';
@@ -6,17 +8,53 @@ import store from './store';
 import Search from './search';
 import Detail from './detail';
 
-const App = () => (
-  <div className="m-3">
-    <Search />
-    <div className="my-3">
-      <Detail />
+const App = withAuth(({ auth }) => {
+  const [authenticated, setAuthenticated] = useState(null);
+
+  useEffect(() => {
+    auth.isAuthenticated().then(isAuthenticated => {
+      if (isAuthenticated !== authenticated) {
+        setAuthenticated(isAuthenticated);
+      }
+    });
+  });
+
+  return (
+    <div className="m-3">
+      {authenticated ? (
+        <>
+          <div className="mb-3">
+            <button className="btn btn-primary" onClick={() => auth.logout()}>
+              Logout
+            </button>
+          </div>
+          <Search />
+          <div className="my-3">
+            <Detail />
+          </div>
+        </>
+      ) : authenticated === null ? (
+        <h4>Loading...</h4>
+      ) : (
+        <button className="btn btn-primary" onClick={() => auth.login()}>
+          Login to search TV Shows
+        </button>
+      )}
     </div>
-  </div>
-);
+  );
+});
 
 export default () => (
   <Provider store={store}>
-    <App />
+    <Router>
+      <Security
+        issuer={`https://${process.env.REACT_APP_OKTA_DOMAIN}/oauth2/default`}
+        client_id={process.env.REACT_APP_OKTA_CLIENT_ID}
+        redirect_uri={`${window.location.origin}/implicit/callback`}
+      >
+        <Route path="/" exact component={App} />
+        <Route path="/implicit/callback" component={ImplicitCallback} />
+      </Security>
+    </Router>
   </Provider>
 );
