9e5ff21d4b364031b9d044a2833d4bf784c18c37
Braden M. Kelley <redbmk@gmail.com>
2019-02-10 19:58:09 -0800

    add hot reloading

--- src/index.js
+++ src/index.js
@@ -11,3 +11,5 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+if (module.hot) module.hot.accept();
